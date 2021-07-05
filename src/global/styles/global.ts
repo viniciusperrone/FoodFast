72a5805e4e2932c8bae05bcd8ec0a3924d170705
Vinicius Perrone <perronevinicius2018@gmail.com>
2021-07-04 21:20:49 -0400

    initial colors added

---
+++ src/global/styles/global.ts
@@ -0,0 +1,22 @@
+export const theme = {
+    colors: {
+
+        button_inside: '#36D2EE',
+        button_around: '#FFFFFF',
+        button: '#F08E67',
+        button_create: '#F39E7C',
+
+        dashboard:'#36D2EE',
+        card: '#F8F8F8',
+        card_green: '#E4FFE5',
+        card_pink: '#FFE5E5',
+        background: '#FFFFFF',
+        modal_background: '#E4E4E4',
+        icons: '#7B7B7B',
+
+        title_default: '#FFFFFF',
+        title: '#25262B',
+        text: '#7B7B7B'
+
+    }
+}
